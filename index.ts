bb3ef9593665ac8f906dc4722e300cf77cf8e964
Smyku6 <smyku6@gmail.com>
2022-01-30 22:52:29 +0100

    types in ts

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
-const add = (a: number | string, b: number | string): number | string => {
-    return a + b;
+const bool: boolean = false;
+
+const liczba: number = 123;
+const liczbab: Number = 123;
+
+const napis: string = 'Abcdef';
+
+const kolor: 'Biały' | 'Żółty' | 'Zielony' = 'Biały';
+
+const tablica: Array<string | number > = ['abc', 'def', 'aaa', 2 , 3];
+
+const tablica2: number[] = [2 , 3];
+
+const tablica3: (string | number)[] = ['abc', 'def', 'aaa', 2 , 3];
+
+enum UserRole {
+    Admin,
+    User,
+    BannedUser,
 }
 
+const bartekUserRole = UserRole.Admin;
 
-console.log(add(2,3));
-console.log(add('Hello','World!'));
+const jajco: number | null =null;
+
